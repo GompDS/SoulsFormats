f641f1d0b9495ecba673cf29ef3e0498dde55686
GompDS <79070939+GompDS@users.noreply.github.com>
2024-11-06 00:21:59 -0800

    base

--- SoulsFormats/Formats/FLVER/FLVER0/Texture.cs
+++ SoulsFormats/Formats/FLVER/FLVER0/Texture.cs
@@ -2,53 +2,18 @@
 {
     public partial class FLVER0
     {
-        /// <summary>
-        /// A single texture map used by a material.
-        /// </summary>
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         public class Texture : IFlverTexture
         {
-            /// <summary>
-            /// Indicates the param name of this texture map which much match to one inside the material file.
-            /// </summary>
-            public string ParamName { get; set; }
+            public string Type { get; set; }
 
-            /// <summary>
-            /// Network path to the texture file; only the filename without extension is actually used.
-            /// </summary>
             public string Path { get; set; }
 
-            /// <summary>
-            /// Create a new empty Texture.
-            /// </summary>
             public Texture()
             {
-                ParamName = string.Empty;
-                Path = string.Empty;
+
             }
 
-            /// <summary>
-            /// Create a new texture with the specified type and path.
-            /// </summary>
-            /// <param name="paramNameicates the type of texture map.</param>
-            /// <param name="path">The name of the texture file.</param>
-            public Texture(string paramName, string path)
-            {
-                ParamName = paramName;
-                Path = path;
-            }
-
-            /// <summary>
-            /// Clone an existing Texture.
-            /// </summary>
-            public Texture(Texture texture)
-            {
-                texture.Path = Path;
-                texture.ParamName = ParamName;
-            }
-
-            /// <summary>
-            /// Read a texture map from a stream.
-            /// </summary>
             internal Texture(BinaryReaderEx br, bool useUnicode)
             {
                 int pathOffset = br.ReadInt32();
@@ -58,14 +23,11 @@
 
                 Path = useUnicode ? br.GetUTF16(pathOffset) : br.GetShiftJIS(pathOffset);
                 if (typeOffset > 0)
-                    ParamName = useUnicode ? br.GetUTF16(typeOffset) : br.GetShiftJIS(typeOffset);
+                    Type = useUnicode ? br.GetUTF16(typeOffset) : br.GetShiftJIS(typeOffset);
                 else
-                    ParamName = null;
+                    Type = null;
             }
 
-            /// <summary>
-            /// Write this texture map to a stream.
-            /// </summary>
             internal void Write(BinaryWriterEx bw, int materialIndex, int textureIndex)
             {
                 bw.ReserveInt32($"Path_Offset{materialIndex}_{textureIndex}");
@@ -74,9 +36,6 @@
                 bw.WriteInt32(0);
             }
 
-            /// <summary>
-            /// Write the strings of this texture map to a stream.
-            /// </summary>
             internal void WriteStrings(BinaryWriterEx bw, int materialIndex, int textureIndex, bool useUnicode)
             {
                 bw.FillInt32($"Path_Offset{materialIndex}_{textureIndex}", (int)bw.Position);
@@ -86,10 +45,11 @@
                     bw.WriteShiftJIS(Path, true);
                 bw.FillInt32($"Type_Offset{materialIndex}_{textureIndex}", (int)bw.Position);
                 if (useUnicode)
-                    bw.WriteUTF16(ParamName, true);
+                    bw.WriteUTF16(Type, true);
                 else
-                    bw.WriteShiftJIS(ParamName, true);
+                    bw.WriteShiftJIS(Type, true);
             }
         }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
     }
 }
